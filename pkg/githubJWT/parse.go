64ac3ce3193a9e92cc43fe6d5b607f443b3bf28f
Chris Marslender <chrismarslender@gmail.com>
2022-10-06 12:42:13 -0500

    Move the jwks endpoint to a const

--- pkg/githubJWT/parse.go
+++ pkg/githubJWT/parse.go
@@ -6,10 +6,14 @@ import (
 	"github.com/chia-network/go-modules/pkg/jwt"
 )
 
+const (
+	githubJWKSEndpoint = "https://token.actions.githubusercontent.com/.well-known/jwks"
+)
+
 // ParseTokenFromRequestWithJWKS Wrapper around the JWT version of this function, that is specific to Github tokens
 // Sets the proper JWKS endpoint, and returns the github specific token data after validation
 func ParseTokenFromRequestWithJWKS(r *http.Request) (*GithubJWT, error) {
-	token, err := jwt.ParseTokenFromRequestWithJWKS(r, "https://token.actions.githubusercontent.com/.well-known/jwks")
+	token, err := jwt.ParseTokenFromRequestWithJWKS(r, githubJWKSEndpoint)
 	if err != nil {
 		return nil, err
 	}
