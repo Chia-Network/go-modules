daffa502a80db1b444f9e69b14146c0a5c6c2898
Chris Marslender <chrismarslender@gmail.com>
2024-02-08 20:05:55 -0600

    Add empty test for now

---
+++ pkg/githubJWT/parse_test.go
@@ -0,0 +1,9 @@
+package githubjwt_test
+
+import (
+	"testing"
+)
+
+func TestTransformTokenToGithubClaims(t *testing.T) {
+	// For now, just adding an empty test so we can also use this Makefile as a template in other repos without`make test` failing CI
+}
