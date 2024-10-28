f1ba3de5c70ce32647a887c167a15871c71b79dd
Chris Marslender <chrismarslender@gmail.com>
2024-10-28 16:02:25 -0500

    Add open with FADV_DONTNEED

--- pkg/directfile/file_linux.go
+++ pkg/directfile/file_linux.go
@@ -25,3 +25,22 @@ func OpenFileWithODirect(path string, blockSize uint32) (*DirectFile, error) {
 
 	return &DirectFile{file: file, blockSize: blockSize, Direct: true}, nil
 }
+
+// OpenFileFADV_DONTNEED opens file with FADV_DONTNEED
+func OpenFileFADV_DONTNEED(path string) (*os.File, error) {
+	// Open the file
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, fmt.Errorf("error opening file: %w", err)
+	}
+
+	// File descriptor
+	fd := int(file.Fd())
+
+	// Use FADV_DONTNEED to suggest not caching pages
+	err = unix.Fadvise(fd, 0, 0, unix.FADV_DONTNEED)
+	if err != nil {
+		fmt.Println("Error setting FADV_DONTNEED:", err)
+		return
+	}
+}
--- pkg/directfile/file_nodirect.go
+++ pkg/directfile/file_nodirect.go
@@ -2,8 +2,17 @@
 
 package directfile
 
+import (
+	"os"
+)
+
 // OpenFileWithODirect Opens a file without system cache (DIRECT)
 func OpenFileWithODirect(path string, blockSize uint32) (*DirectFile, error) {
 	// O_DIRECT is not supported on this platform, fallback to normal open
 	return openNodirect(path)
+}
+
+// OpenFileFADV_DONTNEED opens file with FADV_DONTNEED
+func OpenFileFADV_DONTNEED(path string) (*os.File, error) {
+	return os.Open(path)
 }
